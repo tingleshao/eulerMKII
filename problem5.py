0fbcd1e0d8a9db11cccb5b0897e9947e3aac0dda
tingleshao <tingleshao@gmail.com>
2013-02-15 13:39:30 -0500

    add problem 5

---
+++ problem5.py
@@ -0,0 +1,5 @@
+# project euler problem 5
+# we start with 20, and find the next smaller int, if it has the same factor as previous one,
+# replace it by the other factor.
+# for n = 20, we have:
+20 * 19 * 9 * 17 * 4 * 7 * 13 * 11
