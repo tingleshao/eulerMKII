4879eab9b5a7ad1398b3c317ae4a7d1a39f839be
tingleshao <tingleshao@gmail.com>
2013-02-28 15:14:08 -0500

    problem 26

---
+++ problem21to30/problem26.py
@@ -0,0 +1,34 @@
+# problem 26
+# 997 is the maximum number of stack length python can have, if the question asks a limit >> 1000, it is necessary to come up with a more clever solution.
+
+def detectLoop(i,n, count):
+	i = i * 10
+	lst = ""
+	if count >= 997:
+		return lst
+	if i / n > 0:
+		lst = lst + str(i / n)
+		if i % n != 0:
+			lst = lst + detectLoop(i % n, n, count + 1)
+	else: 
+		lst = lst + '0'
+		lst = lst + detectLoop(i % n , n , count+1)
+	return lst
+looplens = []
+num = []
+for i in range(2,1000):	
+	a = detectLoop(1,i,0)
+	print "0."+ a
+	if len(a) == 997:
+		last = a[-4:]
+		loopLen = len(a.split(last)[1])+4
+		print "------"
+		print "[" + str(i) + "]"
+		print a.split(last)[1]+last
+		print loopLen
+		looplens.append(loopLen)
+		num.append(i)
+print max(looplens)
+print num[looplens.index(982)]
+
+print detectLoop(1,982,0)
