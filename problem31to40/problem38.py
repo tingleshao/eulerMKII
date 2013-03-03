139d14b832cabb0c6ba7b1233162bca778a6f927
tingleshao <tingleshao@gmail.com>
2013-03-03 10:42:14 -0500

    problem 38

---
+++ problem31to40/problem38.py
@@ -0,0 +1,29 @@
+# problem 38 
+def permute(lst):
+	if len(lst) == 1:
+		return lst
+	else:
+		ret_lst = []
+		for i in range(len(lst)):
+			remain = lst[:i]+lst[i+1:]
+			took = lst[i]
+			for g in permute(remain):
+				ret_lst.append(took+g)
+		return ret_lst
+
+# test
+lst = permute('123456789')
+print 'permutation complete'
+n = 98765
+for i in range(len(lst)):
+	ind = len(lst)  - 1
+	new_n = int(lst[ind-i][0:4])
+	if new_n != n:
+		n = new_n
+		print n
+		if ( str(n) + str(n*2) ) in lst:
+			print 'found one'
+			print n
+			break
+		
+		
