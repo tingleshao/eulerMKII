1e9dcf16ab0f5ce6124c5e34f9bec93079df5101
tingleshao <tingleshao@gmail.com>
2013-03-03 22:12:38 -0500

    problem 35

--- problem31to40/problem35.py
+++ problem31to40/problem35.py
@@ -10,7 +10,12 @@ def generatePrime(n):
 					print i
 					p2.remove(i)
 	return p2 
-p2 = generatePrime(1000000)
+
+prime_file = open('millionprimes.txt', 'r')
+prime_lst_str  = prime_file.readline()
+prime_lst = map(int, prime_lst_str.strip('[]').split(", "))
+prime_lst.insert(0,2)
+#p2 = generatePrime(1000000)
 print "generate finished"
 circular = []
 def generateCircular(i):
@@ -19,21 +24,29 @@ def generateCircular(i):
 	len_i = len(str_i)
 	lst = []
 	for i in range(len_i):
-		lst.append(int(str_i[:i]+str_i[i:]))
+		lst.append(int(str_i[i:]+str_i[:i]))
 	return lst
-for p in p2:
+	
+for p in prime_lst:
 	flag = True
-	lst = generateCircular(p)
-	for pp in lst:
-		if pp not in p2:
-			flag = False
+	if '2' in str(p) or '4' in str(p) or '5' in str(p) or '6' in str(p) or '8' in str(p) or '0' in str(p):
+		print str(p) + " is not circular!"
+		flag = False
+	else:
+		lst = generateCircular(p)
+		print lst
+		for pp in lst:
+			if pp not in prime_lst:
+				print str(p) + " is not circular!"
+				flag = False
+				break
 	if flag:
 		print str(p) + "  is circular!"
 		circular.append(p)
 		
 print circular
-print len(circular)
+print len(circular)+2
 
-f = open('millionprimes.txt','w')
-f.write(str(p2))
-f.close()
+#f = open('millionprimes.txt','w')
+#f.write(str(p2))
+#f.close()
