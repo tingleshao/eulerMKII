5da2bef566de3abacd6d74d1716254ab19cd7dde
my ubuntu <tingleshao@gmail.com>
2013-06-06 19:47:29 -0400

    rearrange dir

--- problem101to110/construct_C.m
+++
@@ -1,9 +0,0 @@
-function [ c ] = construct_C( n )
-%CONSTRUCT_C Summary of this function goes here
-%   Detailed explanation goes here
-for i = 1:n-1
-    c(i) = n^(i-1);
-end
-
-end
-
--- problem101to110/u_n.m
+++
@@ -1,4 +0,0 @@
-function [x] = u_n(n)
-    x = 1 - n + n^2 - n^3 + n^4 - n^5 + n^6 - n^7 + n^8 - n^9 + n^10;
-    
-end
